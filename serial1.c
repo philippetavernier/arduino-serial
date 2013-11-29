9dc42704a4c8707f2f029b73c82d73e833ad0731
philippetavernier <philippe.tavernier@gmail.com>
2013-11-29 16:26:14 +0100

    first commit

---
+++ serial1.c
@@ -0,0 +1,18 @@
+/*
+ * Hello World!
+ *
+ * This is the Hello World! for Arduino. 
+ * It shows how to send data to the computer
+ */
+
+
+void setup()                    // run once, when the sketch starts
+{
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+}
+
+void loop()                       // run over and over again
+{
+  Serial.println("Hello world!");  // prints hello with ending line break
+  delay(1000);
+}
---
+++ serial4-bash.c
@@ -0,0 +1,29 @@
+//  BASH
+//      sudo echo -n "0" > /dev/ttyUSB0
+//      sudo echo -n "1" > /dev/ttyUSB0
+//
+
+const int outputPin = 13;
+
+
+void setup()
+{
+  pinMode(outputPin, OUTPUT);
+ // Serial.begin(9600);
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  if (Serial.available() > 0) {
+    int incomingByte = Serial.read();
+    Serial.println(incomingByte, DEC);
+   // if (incomingByte == 49) {
+    if (incomingByte == '1') {
+      digitalWrite(outputPin, HIGH);
+    // if (incomingByte == 49) {
+    } else if (incomingByte == '0') {
+      digitalWrite(outputPin, LOW);
+    }
+  }
+}
